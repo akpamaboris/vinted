dca47eee7e2445285cecdf1f83eee91ab2239c9f
Zeliwipin <zeliwipin@gmail.com>
2021-05-05 16:33:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+//Import components
+import Home from "./containers/Home";
+import Offer from "./containers/Offer";
+
+//Import components end
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        {/* <h1>
+          <Link to="/">Home</Link>
+        </h1>
+        <h1>
+          <Link to="/offer">Offer</Link>
+        </h1> */}
+
+        <Switch>
+          <Route path="/offer:id">
+            <Offer />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/containers/Home.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "../App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+
+import logo from "../vinted-logo.png";
+import banner from "../vinted_banner.jpeg";
+
+function Home() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
+    <div>
+      <header>
+        <img src={logo} alt="Logo of Vinted" />
+        <input type="text" />
+        <button>S'inscrire</button>
+        <button>Se connecter</button>
+        <button>Vends tes articles</button>
+      </header>
+      <div className="banner">
+        <img src={banner} alt="banner of vinted" />
+        <div className="text">
+          <p>Prêts à faire du tri dans vos placards?</p>
+          <button>Commencer à vendre</button>
+        </div>
+      </div>
+      <main>
+        {data.offers.map((x, index) => {
+          return (
+            <Link to="/offer">
+              <div
+                key={index}
+                className="gallery-img"
+                onClick={() => console.log(x)}
+              >
+                <div className="product">
+                  {x.owner.account.username}
+                  <img
+                    className="img-prod"
+                    src={x.product_pictures[0].secure_url}
+                    alt="product"
+                  />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </main>
+    </div>
+  );
+}
+
+export default Home;
