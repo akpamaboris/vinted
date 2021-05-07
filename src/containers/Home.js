ff8121e101d39b7a2f34e3b4a983facd3e2c4fdc
Zeliwipin <zeliwipin@gmail.com>
2021-05-07 22:00:41 +0200

    i added a slider to filter prices on homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,12 +2,48 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "../App.css";
 import { Link } from "react-router-dom";
+import ReactSlider from "react-slider";
+import styled from "styled-components";
 
 import banner from "../vinted_banner.jpeg";
+import React from "react";
+const StyledSlider = styled(ReactSlider)`
+  width: 100%;
+  height: 40px;
+`; // this handles the slider slider
+
+const StyledThumb = styled.div`
+  height: 25px;
+  line-height: 25px;
+  width: 25px;
+  text-align: center;
+  background-color: #000;
+  color: #fff;
+  border-radius: 50%;
+  cursor: grab;
+`;
+
+//this handles the buttons
+
+const Thumb = (props, state) => (
+  <StyledThumb {...props}>{state.valueNow}</StyledThumb>
+);
+
+const StyledTrack = styled.div`
+  top: 0;
+  bottom: 0;
+  background: ${(props) =>
+    props.index === 2 ? "black" : props.index === 1 ? "#0BAEB7" : "#ddd"};
+  border-radius: 999px;
+`;
+
+const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
 
 function Home() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [sliderMinPrice, setSliderMinPrice] = useState(20);
+  const [sliderMaxPrice, setSliderMaxPrice] = useState(50);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -27,6 +63,19 @@ function Home() {
     <span>En cours de chargement...</span>
   ) : (
     <div>
+      <div>
+        <StyledSlider
+          defaultValue={[sliderMinPrice, sliderMaxPrice]}
+          renderTrack={Track}
+          renderThumb={Thumb}
+          onChange={(val) => {
+            setSliderMinPrice(val[0]);
+            setSliderMaxPrice(val[1]);
+          }}
+          max="400"
+          min="1"
+        />
+      </div>
       <div className="banner">
         <img src={banner} alt="banner of vinted" />
         <div className="text">
@@ -36,30 +85,34 @@ function Home() {
       </div>
       <main>
         {data.offers.map((x, index) => {
-          return (
-            <Link
-              key={index}
-              to={{ pathname: `/offer/${index}`, data: { data } }}
-            >
-              <div className="gallery-img">
-                <div className="product">
-                  <div className="bar-img-user">
-                    {x.owner.account.username}
+          return x.product_price >= sliderMinPrice &&
+            x.product_price <= sliderMaxPrice ? (
+            <>
+              <Link
+                key={index}
+                to={{ pathname: `/offer/${index}`, data: { data } }}
+              >
+                <div className="gallery-img">
+                  <div className="product">
+                    <div className="bar-img-user">
+                      {x.owner.account.username}
+                      <img
+                        src={x.owner.account.avatar.secure_url}
+                        alt="user avatar"
+                        className="avatar-img"
+                      />
+                    </div>
                     <img
-                      src={x.owner.account.avatar.secure_url}
-                      alt="user avatar"
-                      className="avatar-img"
+                      className="img-prod"
+                      src={x.product_pictures[0].secure_url}
+                      alt="product"
                     />
+                    <span>{x.product_price} euros</span>
                   </div>
-                  <img
-                    className="img-prod"
-                    src={x.product_pictures[0].secure_url}
-                    alt="product"
-                  />
                 </div>
-              </div>
-            </Link>
-          );
+              </Link>
+            </>
+          ) : null;
         })}
       </main>
     </div>
